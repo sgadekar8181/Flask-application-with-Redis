f1044483931fcbdb3108a80987e1cd5bef792195
Sachin Gadekar <sgadekar8181@gmail.com>
2026-06-04 20:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask
+import os
 import redis
+from flask import Flask
 
 app = Flask(__name__)
 
-r = redis.Redis(
-    host='REDIS_HOST',
-    port=6379,
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
     decode_responses=True
 )
 
-@app.route('/')
+@app.route("/")
 def home():
-    count = r.incr('visits')
-    return f"Total Visits: {count}"
+    count = redis_client.incr("visits")
+    return f"Visits: {count}"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
